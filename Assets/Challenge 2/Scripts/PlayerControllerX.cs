78adc50835a5523fb8060abed6b0e2dd0e499030
joanqueralt94 <salnitre94@gmail.com>
2023-04-10 16:16:30 +0200

    Completed Challenge 2 adding a cooldown timer to the dog spawning

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,24 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float timeElapsed;
+    private float cooldownSpawnTime = 0.5f;
+
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Debug.Log("Updating");
+        if (timeElapsed > cooldownSpawnTime)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timeElapsed = 0f;
+            }
         }
+        else
+        {
+            timeElapsed += Time.deltaTime;
+        }        
     }
 }
