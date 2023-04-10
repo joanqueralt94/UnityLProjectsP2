2034bae12c392eb899cf057398d8d49861490a48
joanqueralt94 <salnitre94@gmail.com>
2023-04-10 16:28:10 +0200

    Adding some values to limit screen movement Vertically

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float playerSpeed = 10.0f;
+
     public float xRange = 10.0f;
+    public float zMinRange = 0f;
+    public float zMaxRange = 15f;
 
     public GameObject projectilePrefab;
 
@@ -29,9 +33,23 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        else if(transform.position.z < zMinRange)
+        {
+            transform.position = new Vector3(zMinRange, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.z > zMaxRange)
+        {
+            transform.position = new Vector3(zMaxRange, transform.position.y, transform.position.z);
+        }
+
+
         //Moving the Player with the Horizontal Unity Input at a certain speed
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime *  playerSpeed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * playerSpeed);
+
 
         //Shooting method every time Space is pressed down
         if(Input.GetKeyDown(KeyCode.Space))
